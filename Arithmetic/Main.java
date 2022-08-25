0911188b1e78da5592426fc60572b32e8ef29f28
Евгений Бубликов <007bublik007@mail.ru>
2022-08-25 07:55:01 +0000

    Upload New File

---
+++ Arithmetic/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        Arithmetic arithmetic = new Arithmetic (8,9);
+
+        Arithmetic.mnozhitelNumber();
+        Arithmetic.slozhenieNumber();
+        Arithmetic.bolshieNumber();
+        Arithmetic.menshieNumber();
+
+
+
+
+
+
+    }
+}
+
