cf34ce71c44b3ca232208c9f266b411cfc824fdc
Бублик <007bublik007@mail.ru>
2022-10-19 16:25:11 +0700

    Learn how to get symbols (variables of type char) by their code (the value of a variable of type int).

--- Primitives/IntOrInteger/src/Main.java
+++ Primitives/IntOrInteger/src/Main.java
@@ -1,8 +1,18 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         Container container = new Container();
         container.addCount(5672);
         System.out.println(container.getCount());
+
+        for (char ch = 'Ё'; ch <= 'ё'; ch++) {
+            if ((ch > 'Ё' && ch < 'А') || (ch > 'я' && ch < 'ё')) {
+                continue;
+            }
+            System.out.println(ch + " - " + ((int) ch));
+        }
+
 
         // TODO: ниже напишите код для выполнения задания:
         //  С помощью цикла и преобразования чисел в символы найдите все коды
