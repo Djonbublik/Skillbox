fc7a61949ebd1576cc1b0db2c4074f6e4f33fe66
Бубликов Евгений <007bublik007@mail.ru>
2023-07-21 09:15:50 +0300

    fixed skills of working with lambda degenerations

--- AdvancedOOPFeatures/homework_2/Airport/src/main/java/Main.java
+++ AdvancedOOPFeatures/homework_2/Airport/src/main/java/Main.java
@@ -1,17 +1,36 @@
 import com.skillbox.airport.Airport;
 import com.skillbox.airport.Flight;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Collections;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
 
+        List<Flight> list = findPlanesLeavingInTheNextTwoHours(Airport.getInstance());
+        list.forEach(System.out::println);
+
 
     }
 
     public static List<Flight> findPlanesLeavingInTheNextTwoHours(Airport airport) {
-        //TODO Метод должден вернуть список рейсов вылетающих в ближайшие два часа.
-        return Collections.emptyList();
-    }
+            //TODO Метод должден вернуть список рейсов вылетающих в ближайшие два часа.
+            List<Flight> list = new ArrayList<>();
+            Calendar calendar = Calendar.getInstance();
+            calendar.add(Calendar.HOUR_OF_DAY, 2);
 
-}
+            airport.getTerminals().forEach(terminal -> terminal.getFlights().forEach(flight -> {
+                if (flight.getDate().after(Calendar.getInstance().getTime())
+                        && flight.getDate().before(calendar.getTime())
+                        && flight.getType() == Flight.Type.DEPARTURE)
+                {
+                    list.add(flight);
+                }
+            }));
+            return list;
+        }
+
+    }
