8971ffb47e8c8fdff44f48d2a42e08f7e8c6cfe5
evgenii_bublikov_1 <007bublik007@mail.ru>
2024-08-06 21:34:10 +0700

    working with files and the network

--- FilesAndNetwork/DataCollector/src/main/java/Main.java
+++ FilesAndNetwork/DataCollector/src/main/java/Main.java
@@ -5,7 +5,6 @@ public class Main {
             System.out.println("Program for parsing files");
 
             Metro mskMetro = new Metro();
-            HtmlParser htmlParser = new HtmlParser(mskMetro);
 
             FinderFilesInFolders finderFiles = new FinderFilesInFolders(mskMetro);
             finderFiles.browsingFolder("data/data");
